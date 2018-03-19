018e0a50f903fc664a69b23508a11770735a5058
Martin <martin.nefjodov@gmail.com>
2018-03-19 10:10:04 +0000

    Project files added.

---
+++ AA_Tech_Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AA_Tech_Test
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Start());
+        }
+    }
+}

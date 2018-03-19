0e9b47729724ee0b0798be6488b30bae0238e770
Martin <martin.nefjodov@gmail.com>
2018-03-19 10:51:54 +0000

    Added the test project.

---
+++ UnitTestProject1/ExcelToolsTests.cs
@@ -0,0 +1,67 @@
+﻿using AA_Tech_Test;
+using AA_Tech_Test.Types;
+using AA_Tech_Test.Exceptions;
+using AA_Tech_Test.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Tests
+{
+    [TestClass]
+    public class ExcelToolsTests
+    {
+        
+        string testFile1 = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "test.xlsx"));
+        string testFile2 = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "test2.xlsx"));
+
+        [TestMethod]
+        public void ReadTest()
+        {
+                        Spreadsheet spreadsheet = ExcelTools.ReadExcelFile(testFile2);
+            if (spreadsheet.Data[0, 0] == "A1" &&
+                spreadsheet.Data[1, 0] == "A2" &&
+                spreadsheet.Data[1, 0] == "A3" &&
+                spreadsheet.Data[2, 0] == "A4" &&
+                spreadsheet.Data[0, 1] == "B1" &&
+                spreadsheet.Data[1, 1] == "B2" &&
+                spreadsheet.Data[1, 1] == "B3" &&
+                spreadsheet.Data[1, 1] == "B4")
+                return;
+
+            throw new Exception("Data read doesn't match data on the test spreadsheet.");
+        }
+
+        public void AppendTest()
+        {
+            File.Copy(testFile1, testFile2, true);
+            Spreadsheet spreadsheet = ExcelTools.ReadExcelFile(testFile2);
+            int code = ExcelTools.AppendToExcelFile("__TEST__", testFile2);
+            switch (code)
+            {
+                case 0:
+                    return;
+                case 1:
+                    throw new InvalidSpreadsheetFormatException();
+                case 2:
+                    throw new NoWriteAccessException();
+            }
+            if (spreadsheet == ExcelTools.ReadExcelFile(testFile2))
+            {
+                throw new NoWriteAccessException();
+            }
+        }
+    }
+}
---
+++ UnitTestProject1/WebFormToolsTests.cs
@@ -0,0 +1,37 @@
+﻿using AA_Tech_Test;
+using AA_Tech_Test.Exceptions;
+using AA_Tech_Test.Types;
+using AA_Tech_Test.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+
+namespace Tests
+{
+    [TestClass]
+    public class WebFormToolsTests
+    {
+        [TestMethod]
+        public void PostFormDataTest()
+        {
+            FormData formData = new FormData("Contact Name", "Contact Email", "Contact Subject", "Message");
+            string reference = WebFormTools.PostFormData(formData);
+            Console.WriteLine($"PostFormDataTest, reference={reference}.");
+            if (reference.Length == 0 || reference.Length > 30)
+            {
+                throw new InvalidReferenceException();
+            }
+        }
+    }
+}

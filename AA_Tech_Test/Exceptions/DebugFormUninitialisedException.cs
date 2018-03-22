1cc96ed69b06cd103eadbc4c97506637b153314a
Martin <martin.nefjodov@gmail.com>
2018-03-22 17:44:56 +0000

    New exception.

---
+++ AA_Tech_Test/Exceptions/DebugFormUninitialisedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AA_Tech_Test.Exceptions
+{
+    public class DebugFormUninitialisedException : Exception
+    {
+        public DebugFormUninitialisedException() 
+            : base(@"The DebugForms.Form was accessed before it was initialised.")
+        {
+        }
+    }
+}
